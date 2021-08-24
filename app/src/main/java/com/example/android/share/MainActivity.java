b3f45870c473b3f2f92b796ebdec57e153534c31
Mithun Parab <mithunprb@gmail.com>
2021-08-24 23:36:03 +0530

    share intent

--- app/src/main/java/com/example/android/share/MainActivity.java
+++ app/src/main/java/com/example/android/share/MainActivity.java
@@ -1,48 +1,31 @@
 package com.example.android.share;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-
-import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText phonenumber,message;
-    Button send;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ActivityCompat.requestPermissions(this,new String[]{
-                Manifest.permission.SEND_SMS},1);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        send=findViewById(R.id.button);
-        phonenumber=findViewById(R.id.editText);
-        message=findViewById(R.id.editText2);
-        send.setOnClickListener((View v) ->  {
+        // Create the text message with a string.
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        //sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage);
+        sendIntent.setType("text/plain");
+
+            // Try to invoke the intent.
+        try {
+            startActivity(sendIntent);
+        } catch (ActivityNotFoundException e) {
+            // Define what your app should do if no activity can handle the intent.
+        }
 
 
-                String number=phonenumber.getText().toString();
-                String msg=message.getText().toString();
-                try {
-                    SmsManager smsManager= SmsManager.getDefault();
-                    smsManager.sendTextMessage(number,null,msg,null,null);
-                    Toast.makeText(getApplicationContext(),"Message Sent", Toast.LENGTH_LONG).show();
-                }catch (Exception e)
-                {
-                    Toast.makeText(getApplicationContext(),e + "Some fiels is Empty",Toast.LENGTH_LONG).show();
-                }
-            }
-        );
     }
 
 }
